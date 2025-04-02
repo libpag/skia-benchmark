5d5d6f7c7b46939551d24311048a18ebc798fa74
billyjin <billyjin@tencent.com>
2025-04-02 15:00:58 +0800

    Remove duplicate boxSizing property setting

--- web/demo/common.ts
+++ web/demo/common.ts
@@ -53,7 +53,6 @@ export function setCanvasDefaultSize(skiaView: SkiaView) {
 
     container.style.backgroundColor = '#2c2c2c';
     canvas.style.border = '10px solid #d2d2d2';
-    canvas.style.boxSizing = 'border-box';
     canvas.style.boxSizing = 'content-box';
     canvas.style.backgroundColor = '#ffffff';
     const scaleFactor = window.devicePixelRatio;
