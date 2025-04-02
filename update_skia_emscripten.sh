a9014c85e032d004d31e5bc392ce973c75cab874
kevingpqi <kevingpqi@tencent.com>
2025-04-02 16:17:58 +0800

    update

--- update_skia_emscripten.sh
+++ update_skia_emscripten.sh
@@ -3,7 +3,7 @@
 ROOT_PATH=$(pwd)
 
 EMSDK_COMMIT="9dbdc4b3437750b85d16931c7c801bb71a782122"
-EMSDK_VERSION="4.0.1"
+EMSDK_VERSION="latest"
 
 EMSDK_COMMIT_LINE="  \"third_party/externals/emsdk\"                  : \"https://skia.googlesource.com/external/github.com/emscripten-core/emsdk.git@${EMSDK_COMMIT}\","
 EMSDK_VERSION_LINE="EMSDK_VERSION = '${EMSDK_VERSION}'"
